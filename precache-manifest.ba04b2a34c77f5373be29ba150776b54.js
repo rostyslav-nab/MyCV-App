962f0e2344094723f0b000b51b69bdc1e0e22459
rostyslav-nab <rostuslav.nab@gmail.com>
2020-03-22 01:53:14 +0200

    Updates

---
+++ precache-manifest.ba04b2a34c77f5373be29ba150776b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e89936689d70953bda5ecf707ba377",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "89ab16f1dda9362455f0",
+    "url": "/my-portfolio/static/css/main.047472f4.chunk.css"
+  },
+  {
+    "revision": "299cf37898bf0218cdde",
+    "url": "/my-portfolio/static/js/2.bfdd84bb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/my-portfolio/static/js/2.bfdd84bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ab16f1dda9362455f0",
+    "url": "/my-portfolio/static/js/main.de0b6c9f.chunk.js"
+  },
+  {
+    "revision": "d4a6f897414192edb936",
+    "url": "/my-portfolio/static/js/runtime-main.7e7d7162.js"
+  }
+]);
